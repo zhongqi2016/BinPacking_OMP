9647a50c7b9bc59b9f2331c88d32cf27fc60e0e9
WU ZHONGQI <yzqwzq@qq.com>
2022-02-11 03:35:38 +0300

    .

--- main.cpp
+++ main.cpp
@@ -6,26 +6,37 @@ using namespace std;
 
 
 int main() {
-    int num_threads = 8; //Set number of threads
     string path = "./bin1data/"; //Path of data
+    //The number of threads to be tested
+    const array<int, 4> threads = {2};
 
-    omp_set_num_threads(num_threads);
     ReadFiles readFiles(path);
     readFiles.sortDirs();
+
     for (int i = 0; i < readFiles.getNumOfFiles(); ++i) {
-        cout << i << ". " << readFiles.getFileName(i)<<endl;
+        cout << i << ". " << readFiles.getFileName(i);
         BinPacking binPacking = readFiles.getData(i);
-        double start, end;
-        start = omp_get_wtime();
+        int result = -1;
+        for (auto th:threads) {
+            omp_set_num_threads(th);
+            double start, end;
+            start = omp_get_wtime();
 
-        int result = binPacking.BNB();
-        int countBranches=binPacking.getCountBranches();
-        end = omp_get_wtime();
-        double time = (end - start);
-        cout<<"solution: ";
-        binPacking.printSolution2();
-        printf("result=%d, time=%lf, branches=%d\n", result,time,countBranches);
+            int res = binPacking.BNB();
+            int countBranches = binPacking.getCountBranches();
+            end = omp_get_wtime();
+            double time = (end - start);
 
+            if (result == -1) {
+                result = res;
+            } else if (result != res) {
+                printf(" error!");
+            }
+//            cout<<"solution: ";
+//            binPacking.printSolution2();
+            printf(" %lf %d", time, countBranches);
+        }
+        printf(" %d\n",result);
     }
 
     return 0;
